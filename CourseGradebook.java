6dd83cb305177e51a56d5b8f70cb1568464def6c
hquantz <95895131+hquantz@users.noreply.github.com>
2022-02-01 14:53:39 -0700

    Create CourseGradebook.java

---
+++ CourseGradebook.java
@@ -0,0 +1,7 @@
+private int gradeBook[][];
+private int numberOfStudents;
+private int numberOfTests;
+
+public CourseGradebook(int numberOfStudents, int numberOfTests){
+  this.numberOfStudents = numberOfStudents
+}
